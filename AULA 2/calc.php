bf1c7271b3cca927d2f85167cbdc43abc4f69999
joao <joao>
2024-08-27 10:29:10 -0300

    Atualizando calc

---
+++ AULA 2/calc.php
@@ -0,0 +1,83 @@
+<?php
+    $v1 = $_GET["a"];
+    $v2 = $_GET["b"];
+    $op = $_GET["op"];
+
+    $result = 0;
+
+    if ($v2 == NULL){
+        $v2 = 0;
+    }
+    if ($v1 == NULL){
+        $v1 = 0;
+    }
+
+    switch ($op)
+    {
+        case "+":
+            $result = $v1 + $v2;
+            break;
+
+        case "-":
+            $result = $v1 - $v2;
+            break;
+
+        case "*":
+            $result = $v1 * $v2;
+            break;
+
+        case "/":
+            $result = $v1 / $v2;
+            break;
+        
+        case "sqrt":
+            $result = sqrt($v1);
+            $result2 = sqrt($v2);
+            break;
+
+        case "+/-":
+            $result = $v1 *(-1);
+            $result2 = $v2 *(-1);
+            break;
+
+        case "1/x":
+            $result = 1 / $v1;
+            $result2 = 1 / $v2;
+            break;
+
+        case "sen":
+            $result = sin(deg2rad($v1));
+            $result2 = sin(deg2rad($v2));
+            break;
+
+        case "cos":
+            $result = cos(deg2rad($v1));
+            $result2 = cos(deg2rad($v2));
+            break;
+
+        case "tan":
+            $result = tan(deg2rad($v1));
+            $result2 = tan(deg2rad($v2));
+            break;
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        if(($op == "+") || ($op == "-") || ($op == "/") || ($op == "*"))
+        {
+            echo "<h1>$op: $result</h1>"; 
+        }else{
+            echo "<h1>A: $result</h1><br><h1>B: $result2</h1>"; 
+        } 
+    ?>
+</body>
+</html>
