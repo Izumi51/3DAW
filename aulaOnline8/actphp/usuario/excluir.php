18158e40f25a5c389447f09ba6ab404b0ddb1fe4
Joao <joaoizumi27@gmail.com>
2024-11-05 11:31:26 -0300

    erro teste

---
+++ aulaOnline8/actphp/usuario/excluir.php
@@ -0,0 +1,21 @@
+<?php
+    $servidor = "localhost"; 
+    $user = "root";
+    $senha = "";
+    $database = "perguntas";
+
+    $conexao = new mysqli ($servidor, $user, $senha, $database);
+
+    if ($conexao->connect_error) {  
+        die ("Conexao Falhou!");
+    } else {   
+        if ($_SERVER["REQUEST_METHOD"] == "POST") 
+        {
+            $id = $_POST["excUsua"];
+
+            $comando = "DELETE FROM `usuario` WHERE `usuario`.`id` = " . $id;
+            
+            $resultado = $conexao->query($comando);
+        }
+    }
+?>
--- aulaOnline8/temp.js
+++ aulaOnline8/temp.js
@@ -1,4 +1,5 @@
-function incluir() {
+/*Scripts para as PERGUNTAS*/
+function incluirPergunta() {
     let questao = document.getElementById("pergunta").value;
     let tipo = document.getElementById("tipo").value;
     let assunto = document.getElementById("assunto").value;
@@ -15,27 +16,83 @@ function incluir() {
         }
     }
 
-    xmlhttp.open("POST", "http://localhost/3DAW/aulaOnline8/actphp/incluir.php");
+    xmlhttp.open("POST", "http://localhost/3DAW/aulaOnline8/actphp/pergunta/incluir.php");
     xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xmlhttp.send("questao=" + questao + "&assunto=" + assunto + "&tipo=" + tipo);
 }
 
-function alterar() {
+function alterarPergunta() {
 
 }
 
-function excluir() {
+function excluirPergunta() {
 
 }
 
-function lstTodas() {
+function lstTodasPerguntas() {
 
 }
 
-function lstUm() {
+function lstUmaPergunta() {
 
 }
 
+/*Scripts para o USUARIO*/
+function incluirUsuario() {
+    let nome = document.getElementById("nome").value;
+    let email= document.getElementById("email").value;
+    let senha= document.getElementById("senha").value;
+    
+    let xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log("Chegou a resposta OK: " + this.responseText);
+        } else if (this.readyState < 4) {
+            console.log("3: " + this.readyState);
+        } else {
+            console.log("Requisicao falhou: " + this.status);
+        }
+    }
+
+    xmlhttp.open("POST", "http://localhost/3DAW/aulaOnline8/actphp/usuario/incluir.php");
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send("nome=" + nome + "&email=" + email + "&senha=" + senha);
+}
+
+function alterarUsuario() {
+
+}
+
+function excluirUsuario() {
+    let excUsua = document.getElementById("excUsua").value;
+    
+    let xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log("Chegou a resposta OK: " + this.responseText);
+        } else if (this.readyState < 4) {
+            console.log("3: " + this.readyState);
+        } else {
+            console.log("Requisicao falhou: " + this.status);
+        }
+    }
+    
+    xmlhttp.open("POST", "http://localhost/3DAW/aulaOnline8/actphp/usuario/excluir.php");
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send("excUsua=" + excUsua);
+}
+
+function lstTodosUsuarios() {
+
+}
+
+function lstUmUsuario() {
+
+}
+
+/*Scripts para a VISIBILIDADE*/
 function menuTest(type) {
     switch (type) {
         case 1:
@@ -58,11 +115,31 @@ function menuTest(type) {
             displayReset();
             document.getElementById("5").style.display = "block";
             break;
+        case 6:
+            displayReset();
+            document.getElementById("6").style.display = "block";
+            break;
+        case 7:
+            displayReset();
+            document.getElementById("7").style.display = "block";
+            break;
+        case 8:
+            displayReset();
+            document.getElementById("8").style.display = "block";
+            break;
+        case 9:
+            displayReset();
+            document.getElementById("9").style.display = "block";
+            break;
+        case 10:
+            displayReset();
+            document.getElementById("10").style.display = "block";
+            break;
     }
 }
 
 function displayReset() {
-    for (var i = 1; i <= 5; i++) {
+    for (var i = 1; i <= 10; i++) {
         var element = document.getElementById(i.toString());
         if (element) {
             element.style.display = 'none'; 
